09ae80794d66496d27723ec22de83d4a1ebe85ab
John Eckert <john2023219@live.ytech.edu>
2021-09-17 08:45:48 -0400

    Update team.js

--- team.js
+++ team.js
@@ -3,11 +3,11 @@ function homeButton() {
 }
 
 function aboutButton() {
-
+window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
 
 function johnEckert() {
-
+alert "close this if you're stupid";
 }
 
 function jaysinH() {
